14f9ec49727670c02a8033341edd0cc8e1ce4548
Óscar Paz <19271035+OscarPaz@users.noreply.github.com>
2020-10-14 22:00:35 +0200

    Edited build config file

--- config.mk
+++ config.mk
@@ -1,11 +1,10 @@
 # dwm version
 VERSION = 6.2
 
-# Customize below to fit your system
-
 # paths
-PREFIX = /usr/local
-MANPREFIX = ${PREFIX}/share/man
+LOCAL = $(HOME)/.local/share
+PREFIX = $(LOCAL)/dwm
+MANPREFIX = ${LOCAL}/man
 
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
